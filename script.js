4f1a6545eafef847a0cb0f40a2991a27d6619176
bentononlouie-collab <bentononlouie@gmail.com>
2026-07-19 15:12:49 +0800

    First Upload

--- script.js
+++ script.js
@@ -28,3 +28,26 @@ function toggleMenu(){
             }
         });
     }
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".NavLinks a");
+window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.offsetHeight;
+
+        if(window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight){
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        const text = link.querySelector("h1");
+            text.classList.remove("active");
+                if(link.getAttribute("href") === "#" + current){
+                    text.classList.add("active");
+                    console.log(text.className);
+                }
+            });
+    });
